ed7c9f3ecdf7e950efc17ee83680f370b80ad76f
rexielnino <kaszalakristof1@gmail.com>
2020-11-18 16:36:09 +0100

    Uploaded the project

---
+++ src/main/java/com/userfront/service/ServiceImplementation/AppointmentServiceImpl.java
@@ -0,0 +1,38 @@
+package com.userfront.service.ServiceImplementation;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.userfront.dao.AppointmentDao;
+import com.userfront.domain.Appointment;
+import com.userfront.service.AppointmentService;
+
+@Service
+public class AppointmentServiceImpl implements AppointmentService {
+
+    @Autowired
+    private AppointmentDao appointmentDao;
+
+    public Appointment createAppointment(Appointment appointment) {
+       return appointmentDao.save(appointment);
+    }
+
+    public List<Appointment> findAll() {
+        return appointmentDao.findAll();
+    }
+
+    public Appointment findAppointment(Long id) {
+    	Optional<Appointment> appointment = appointmentDao.findById(id);
+    	return appointment.get();
+
+    }
+
+    public void confirmAppointment(Long id) {
+        Appointment appointment = findAppointment(id);
+        appointment.setConfirmed(true);
+        appointmentDao.save(appointment);
+    }
+}
